b2151b28620714f619f1371394fe1a54421d0242
RedX997 <arunpattar13503@gmail.com>
2026-04-18 10:33:25 +0530

    chore: remove redundant nested 'Service pro' directory and consolidate files

--- Service pro/management practice/server/migrations/add-employee-role.sql
+++
@@ -1,2 +0,0 @@
--- Add employee role (safe to run multiple times)
-INSERT INTO roles (role_name) VALUES ('employee') ON CONFLICT (role_name) DO NOTHING;
--- Service pro/management practice/src/components/filters/index.ts
+++
@@ -1,5 +0,0 @@
-export { FilterBar } from './FilterBar';
-export { FilterTextInput } from './FilterTextInput';
-export { FilterDropdown } from './FilterDropdown';
-export { FilterMultiSelect } from './FilterMultiSelect';
-export { FilterNumberRange } from './FilterNumberRange';
