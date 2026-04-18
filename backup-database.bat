855677f832bafdf2484838350dfaae4bba86e366
RedX997 <arunpattar13503@gmail.com>
2026-04-18 10:28:55 +0530

    chore: remove unwanted development guides, scripts, and temporary files

--- backup-database.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Creating database backup...
-pg_dump -h localhost -U postgres -d servicepro > servicepro_backup_%date:~-4,4%%date:~-10,2%%date:~-7,2%.sql
-echo Backup created: servicepro_backup_%date:~-4,4%%date:~-10,2%%date:~-7,2%.sql
-pause
